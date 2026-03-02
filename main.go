2ccc9ba2dcb573c1409c2c59dcb654921a471fcc
Matthew Baldeosingh <baldeosinghsm@gmail.com>
2026-03-02 14:28:44 -0500

    feat: Implement create event and get event features; also implemented http request testing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,12 +14,34 @@ func main() {
 	// Create Gin HTTP server w/ default middleware (logging + recovery)
 	server := gin.Default()
 
-	server.GET("/events", getEvents) // Create /events endpoint
+	server.GET("/events", getEvents) // Create "/events" endpoint; make getEvents the endpoint handler
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // Run server on port 8080
 }
 
 func getEvents(context *gin.Context) {
-	// Return JSON response
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events := models.GetAllEvents()
+	// Gin package automatically transforms data into JSON
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	// Bind user request to the above event variable
+	err := context.ShouldBindJSON(&event) // func needs a pointer to the object, event
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	// Create temporary dummy IDs
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	// Send back OK status code and the event that was created
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
